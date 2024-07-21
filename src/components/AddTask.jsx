6afda0d67b173e6ab04fdb5c9841d18743036365
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-21 14:26:07 +0300

    Create AddTask.jsx

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AddTask = () => {
+  return (
+    <>
+      <button
+        className="bg-blue-600 text-white px-2 py-1 rounded-lg
+         uppercase font-semibold text-sm"
+        type=""
+      >
+        +New
+      </button>
+    </>
+  );
+};
+
+export default AddTask;
