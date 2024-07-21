de982e15c2d7be415688eda5a0bc11a62b255670
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-21 14:26:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import AddTask from "./components/AddTask";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold text-center my-3 py-4 px-6">
         Kanban Task Tracker
       </h1>
-      <p className="text-xl px-6">Hi there</p>
-      <p className="text-xl px-6">Click +New to add a new task</p>
+
+      <p className="text-xl px-6">
+        <span>Hi there </span>
+        <span>Click </span>
+        <AddTask />
+        <span> to add a new task</span>
+      </p>
     </>
   );
 }
