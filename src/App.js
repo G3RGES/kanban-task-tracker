b8b8a4231884b07562cd2baea8f42331597539af
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-21 16:31:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [taskList, setTaskList] = useState();
+
   return (
     <>
       <h1 className="text-3xl font-bold text-center my-3 py-4 px-6">
         Kanban Task Tracker
       </h1>
-      <div className="flex flex-row">
-        <p className="text-xl pl-1">Hi there, </p>
-        <p className="text-xl pl-1"> Click </p>
+      <p className="text-xl pl-6">Hi there, </p>
+      <div className="flex flex-row items-center ">
+        <p className="text-xl pl-6"> Click </p>
         <AddTask />
-        <p className="text-xl pl-1">to add a new task </p>
+        <p className="text-xl my-2">to add a new task </p>
       </div>
     </>
   );
