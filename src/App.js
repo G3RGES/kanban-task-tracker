5e4325f84d95c7ee6b681ffcf48794bf7fbee7b0
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-22 14:01:47 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Todo from "./components/Todo";
+import { useDrop } from "react-dnd";
 
 function App() {
   const [taskList, setTaskList] = useState([]);
+  const [completed, setCompleted] = useState([]);
 
   useEffect(() => {
     let array = localStorage.getItem("taskList");
@@ -12,6 +14,35 @@ function App() {
       setTaskList(JSON.parse(array));
     }
   }, []);
+
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "todo",
+    drop: (item) =>
+      addToCompleted(
+        item.id,
+        item.projectName,
+        item.taskDescription,
+        item.timestamp,
+        item.duration
+      ),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
+
+  const addToCompleted = (
+    id,
+    projectName,
+    taskDescription,
+    timestamp,
+    duration
+  ) => {
+    const moveTask = taskList.filter((task) => id === task.id);
+    setCompleted((completed) => [
+      ...completed,
+      { moveTask, projectName, taskDescription, timestamp, duration },
+    ]);
+  };
 
   return (
     <>
@@ -32,7 +63,29 @@ function App() {
           >
             To Do:
           </h2>
-          {taskList.map((task, idx) => (
+
+          <div className="ml-6 flex flex-col-reverse">
+            {taskList.map((task, idx) => (
+              <Todo
+                // key={new Date().getMilliseconds().toPrecision() + Math.random()}
+                key={idx}
+                task={task}
+                index={idx}
+                taskList={taskList}
+                setTaskList={setTaskList}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="w-full flex flex-col" ref={drop}>
+          <h2
+            className="bg-[#D4BDAC] my-4 text-xl w-3/4 max-w-lg font-semibold text-center
+        rounded-lg py-2 px-4"
+          >
+            Completed:
+          </h2>
+          {completed.map((task, idx) => (
             <Todo
               // key={new Date().getMilliseconds().toPrecision() + Math.random()}
               key={idx}
@@ -42,15 +95,6 @@ function App() {
               setTaskList={setTaskList}
             />
           ))}
-        </div>
-
-        <div className="w-full">
-          <h2
-            className="bg-[#D4BDAC] my-4 ml-6 text-xl w-3/4 max-w-lg font-semibold text-center
-        rounded-lg py-2 px-4"
-          >
-            Completed:
-          </h2>
         </div>
       </div>
     </>
