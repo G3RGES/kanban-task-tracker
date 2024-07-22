171ef558bcc015212c5d4faeb00e81afbd9ba3bf
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-07-22 11:58:34 +0300

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EditTask from "./EditTask";
 
 const Todo = ({ task, index, taskList, setTaskList }) => {
+  const [time, setTime] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+
+    if (running) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else if (!running) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   const handleDelete = (itemID) => {
     let removeIndex = taskList.indexOf(task);
     taskList.splice(removeIndex, 1);
@@ -27,6 +43,47 @@ const Todo = ({ task, index, taskList, setTaskList }) => {
           />
         </div>
         <p className="text-lg py-2 ">{task.taskDescription}</p>
+
+        <div className="w-full flex flex-row items-center justify-evenly ">
+          <div className="w-1/4 text-xl font-semibold py-4 ">
+            <span className="">
+              {("0" + Math.floor((time / 360000) % 24)).slice(-2)}:
+            </span>
+            <span className="">
+              {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+            </span>
+            <span className="">
+              {("0" + Math.floor((time / 1000) % 60)).slice(-2)}
+            </span>
+            <span className="text-sm">
+              :{("0" + ((time / 10) % 100)).slice(-2)}
+            </span>
+          </div>
+
+          <div className=" flex flex-row justify-evenly  gap-4">
+            {running ? (
+              <button
+                className="border rounded-lg py-1 px-3 "
+                onClick={() => setRunning(false)}
+              >
+                Stop
+              </button>
+            ) : (
+              <button
+                className="border rounded-lg py-1 px-3 "
+                onClick={() => setRunning(true)}
+              >
+                Start
+              </button>
+            )}
+            <button
+              className="border rounded-lg py-1 px-3 bg-[#B43F3F] text-[#f9f6ee]"
+              onClick={() => setTime(0)}
+            >
+              Reset
+            </button>
+          </div>
+        </div>
 
         <div className="w-full flex flex-row justify-center">
           <button
